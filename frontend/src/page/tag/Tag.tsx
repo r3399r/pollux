a670ca8c96222bec77e5026ab8ebc8174720571f
Yue <yue@potentia.asia>
2022-10-18 11:54:26 +0800

    sync submodule

--- frontend/src/page/tag/Tag.tsx
+++ frontend/src/page/tag/Tag.tsx
@@ -1,8 +1,11 @@
+import Button from 'src/component/celestial-ui/Button';
 import H1 from 'src/component/celestial-ui/typography/H1';
 
 const Tag = () => (
   <>
     <H1>標籤清單</H1>
+    <Button className="my-3">新增標籤</Button>
+    <div>tags</div>
   </>
 );
 
