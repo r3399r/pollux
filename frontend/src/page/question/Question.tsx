2048f3b9f08e61255192694ec64cf3a830eea3cb
Yue <yue@potentia.asia>
2022-10-25 18:00:02 +0800

    introduce ckeditor5

--- frontend/src/page/question/Question.tsx
+++ frontend/src/page/question/Question.tsx
@@ -1,3 +1,19 @@
-const Question = () => <div>題目清單</div>;
+import { useNavigate } from 'react-router-dom';
+import Button from 'src/component/celestial-ui/Button';
+import H1 from 'src/component/celestial-ui/typography/H1';
+import { Page } from 'src/constant/Page';
+
+const Question = () => {
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <H1>題目清單</H1>
+      <Button className="my-3" onClick={() => navigate(`${Page.Question}/edit`)}>
+        新增題目
+      </Button>
+    </>
+  );
+};
 
 export default Question;
--- frontend/src/page/questionEdit/QuestionEdit.tsx
+++ frontend/src/page/questionEdit/QuestionEdit.tsx
@@ -1,3 +1,33 @@
-const QuestionEdit = () => <div>增修題目</div>;
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import H1 from 'src/component/celestial-ui/typography/H1';
+
+const QuestionEdit = () => {
+  ClassicEditor.create;
+
+  return (
+    <>
+      <H1>增修題目</H1>
+      <CKEditor
+        editor={ClassicEditor}
+        data="<p>Hello from CKEditor 5!</p>"
+        onReady={(editor) => {
+          // You can store the "editor" and use when it is needed.
+          console.log('Editor is ready to use!', editor);
+        }}
+        onChange={(event, editor) => {
+          const data = editor.getData();
+          console.log({ event, editor, data });
+        }}
+        onBlur={(event, editor) => {
+          console.log('Blur.', editor);
+        }}
+        onFocus={(event, editor) => {
+          console.log('Focus.', editor);
+        }}
+      />
+    </>
+  );
+};
 
 export default QuestionEdit;
