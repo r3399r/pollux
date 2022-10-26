546c241b4b7043f4f57499459a29660b4c6c9c1f
Yue <yue@potentia.asia>
2022-10-26 11:46:42 +0800

    add Switch

--- frontend/src/page/questionEdit/QuestionEdit.tsx
+++ frontend/src/page/questionEdit/QuestionEdit.tsx
@@ -1,10 +1,18 @@
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
+import { useState } from 'react';
+import Input from 'src/component/celestial-ui/Input';
+import Switch from 'src/component/celestial-ui/Switch';
 import H1 from 'src/component/celestial-ui/typography/H1';
 import H3 from 'src/component/celestial-ui/typography/H3';
 
 const QuestionEdit = () => {
-  ClassicEditor.create;
+  const [content, setContent] = useState<string>();
+  const [hasAnswer, setHasAnswer] = useState<boolean>(true);
+  const [answer, setAnswer] = useState<string>();
+  const [hasSolution, setHasSolution] = useState<boolean>(false);
+  const [solution, setSolution] = useState<string>();
+  console.log(content, answer, solution);
 
   return (
     <>
@@ -13,42 +21,33 @@ const QuestionEdit = () => {
       <H3 className="mb-4">題目內容</H3>
       <CKEditor
         editor={ClassicEditor}
-        data="<p>Hello from CKEditor 5!</p>"
-        onReady={(editor) => {
-          // You can store the "editor" and use when it is needed.
-          console.log('Editor is ready to use!', editor);
-        }}
         onChange={(event, editor) => {
           const data = editor.getData();
-          console.log({ event, editor, data });
-        }}
-        onBlur={(event, editor) => {
-          console.log('Blur.', editor);
-        }}
-        onFocus={(event, editor) => {
-          console.log('Focus.', editor);
+          setContent(data);
         }}
       />
-      <H3 className="my-4">簡答</H3>
-      <H3 className="mb-4">詳解</H3>
-      <CKEditor
-        editor={ClassicEditor}
-        data="<p>Hello from CKEditor 5!</p>"
-        onReady={(editor) => {
-          // You can store the "editor" and use when it is needed.
-          console.log('Editor is ready to use!', editor);
-        }}
-        onChange={(event, editor) => {
-          const data = editor.getData();
-          console.log({ event, editor, data });
-        }}
-        onBlur={(event, editor) => {
-          console.log('Blur.', editor);
-        }}
-        onFocus={(event, editor) => {
-          console.log('Focus.', editor);
-        }}
+      <div className="flex items-center mt-4 gap-4">
+        <H3>答案</H3>
+        <Switch onChange={(e) => setHasAnswer(e.target.checked)} defaultChecked />
+      </div>
+      <Input
+        onChange={(event) => setAnswer(event.target.value)}
+        disabled={!hasAnswer}
+        placeholder="請輸入答案 (僅一行)"
       />
+      <div className="flex items-center my-4 gap-4">
+        <H3>詳解</H3>
+        <Switch onChange={(e) => setHasSolution(e.target.checked)} />
+      </div>
+      {hasSolution && (
+        <CKEditor
+          editor={ClassicEditor}
+          onChange={(event, editor) => {
+            const data = editor.getData();
+            setSolution(data);
+          }}
+        />
+      )}
     </>
   );
 };
