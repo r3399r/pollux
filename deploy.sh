c0622d1497d7252b1248d9ad84b74add85289304
Yue <yue@potentia.asia>
2023-06-09 10:47:43 +0800

    move folder

--- deploy.sh
+++ deploy.sh
@@ -13,14 +13,12 @@ echo domain: $subDomain.$domain
 echo ====================================================================================
 
 echo deploy backend AWS...
-cd ../backend
 aws cloudformation package --template-file aws/cloudformation/template.yaml --output-template-file packaged.yaml --s3-bucket y-cf-midway-singapore
 aws cloudformation deploy --template-file packaged.yaml --stack-name $project-$env-stack --parameter-overrides TargetEnvr=$env Project=$project SubDomain=$subDomain Domain=$domain --no-fail-on-empty-changeset --s3-bucket y-cf-midway-singapore
 echo ====================================================================================
 
 echo deploy frontend to S3...
-cd ../frontend
-npm ci
+npm i
 npm run pre:deploy
 aws s3 sync ./dist s3://$project-$env-y --delete --cache-control no-cache
 echo ====================================================================================
