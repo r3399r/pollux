84e0540f282fbfd8828f05c9237d9267810800e9
Yue <yue@potentia.asia>
2023-06-19 18:18:20 +0800

    add Drawer

--- src/component/Menu.tsx
+++ src/component/Menu.tsx
@@ -1,14 +1,34 @@
+import classNames from 'classnames';
 import { useNavigate } from 'react-router-dom';
 import { Type, TypeName } from 'src/model/Common';
 import Body from './typography/Body';
 
-const Menu = () => {
+type Props = {
+  isDrawer?: boolean;
+  onClick?: () => void;
+};
+
+const Menu = ({ isDrawer = false, onClick }: Props) => {
   const navigate = useNavigate();
 
   return (
-    <div className="flex flex-col gap-[5px] rounded-[15px] bg-olive-500 h-full text-white px-[10px] py-5 overflow-y-auto">
+    <div
+      className={classNames(
+        'flex flex-col gap-[5px] bg-olive-500 h-full text-white px-[10px] py-5 overflow-y-auto',
+        {
+          'rounded-[15px]': !isDrawer,
+        },
+      )}
+    >
       {Object.values(Type).map((v, i) => (
-        <Body key={i} onClick={() => navigate(`/${v}`)} className="cursor-pointer py-2 px-5">
+        <Body
+          key={i}
+          onClick={() => {
+            onClick && onClick();
+            navigate(`/${v}`);
+          }}
+          className="cursor-pointer py-2 px-5"
+        >
           {TypeName[v]}
         </Body>
       ))}
