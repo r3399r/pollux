76c5383f3d986030fd3cd465121ac279d309f9c3
Yue <yue@potentia.asia>
2023-06-29 16:12:35 +0800

    fix rationalize and direct homt

--- src/util/factory.ts
+++ src/util/factory.ts
@@ -187,22 +187,31 @@ const rationalize = (): Question => {
   let a = { numeratorCoefficient: 1, numeratorRadical: 1, denominator: 1 };
   let q = '';
 
-  while (denominator === numerator || a.numeratorRadical === 1) {
-    denominator = randomIntBetween(2, 20);
-    numerator = randomIntBetween(1, 20);
-    a = rationalizeSingle(denominator, numerator);
-  }
-
   // 1: sqrt{a}/sqrt{b}, 2: sqrt{a/b}, 3: a/sqrt{b}
   const type = randomIntBetween(1, 3);
   switch (type) {
     case 1:
+      while (a.numeratorRadical === 1 || a.denominator === 1) {
+        denominator = randomIntBetween(2, 20);
+        numerator = randomIntBetween(1, 20);
+        a = rationalizeSingle(denominator, numerator);
+      }
       q = `化簡 \\(\\dfrac{\\sqrt{${numerator}}}{\\sqrt{${denominator}}}=\\dfrac{a\\sqrt b}{c}\\)`;
       break;
     case 2:
+      while (a.numeratorRadical === 1 || a.denominator === 1) {
+        denominator = randomIntBetween(2, 20);
+        numerator = randomIntBetween(1, 20);
+        a = rationalizeSingle(denominator, numerator);
+      }
       q = `化簡 \\(\\sqrt{\\dfrac{${numerator}}{${denominator}}}=\\dfrac{a\\sqrt b}{c}\\)`;
       break;
     case 3:
+      while (a.numeratorRadical === 1 || a.denominator === 1) {
+        denominator = randomIntBetween(2, 20);
+        numerator = randomIntBetween(1, 20);
+        a = rationalizeSingle(denominator, numerator * numerator);
+      }
       q = `化簡 \\(\\dfrac{${numerator}}{\\sqrt{${denominator}}}=\\dfrac{a\\sqrt b}{c}\\)`;
       break;
   }
